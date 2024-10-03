943f9b57a2460a54c41e01a5faaeaa86723f9687
shahji01 <ushahfaisalranta@gmail.com>
2024-10-03 08:55:32 -0700

    fix

---
+++ database/migrations/2024_10_03_155212_create_sub_topics_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubTopicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_topics', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('topic_id');
+            $table->string('name');
+            $table->integer('status')->default(1)->comment('1 = Active, 2 = Inactive');
+            $table->string('created_by');
+            $table->date('created_date');
+            $table->date('created_time');
+            $table->string('updated_by')->nullable();
+            $table->date('updated_date')->nullable();
+            $table->string('updated_time')->nullable();
+
+            $table->foreign('topic_id')
+                ->references('id')
+                ->on('topics')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_topics');
+    }
+}
---
+++ database/migrations/2024_10_03_155343_create_objectives_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateObjectivesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('objectives', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('sub_topic_id');
+            $table->string('name');
+            $table->integer('status')->default(1)->comment('1 = Active, 2 = Inactive');
+            $table->string('created_by');
+            $table->date('created_date');
+            $table->date('created_time');
+            $table->string('updated_by')->nullable();
+            $table->date('updated_date')->nullable();
+            $table->string('updated_time')->nullable();
+            
+            $table->foreign('sub_topic_id')
+                ->references('id')
+                ->on('sub_topics')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('objectives');
+    }
+}
